66a910e07908b1eeeeee9bedcb3efccd769564eb
Leonardo Brunholi <leonardo.brunholi@eaportal.org>
2024-03-21 17:49:00 -0300

    segundo commit do JS

--- script.js
+++ script.js
@@ -14,11 +14,24 @@ pessoasInput.addEventListener ("input", receberQuantidadePessoas)
 function receberQuantidadePessoas(evento) {
     const paragrafoErro = document.querySelector(".pessoa #erro")
     const divErro = document.querySelector(".pessoa .input-box")
+
     if(evento.target.value === "0"){
         paragrafoErro.getElementsByClassName.display = "block"
-
         divErro.setAttribute("id", "erro-div")
     } else {
-        
+        paragrafoErro.getElementsByClassName.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.tanget.value)
     }
+}
+    const botoesGorjeta = document.querySelectorAll("-gorjeta input[type='button']")
+    botoesGorjeta. forEach(botao => {
+        botao.addEventListener ("click", receberPorcentagem)
+})
+
+    function receberPorcentagem (evento) {
+        console. log(evento.target.value)
+
+        // porcentagem = Number (evento. target. value) / 100
+       // console. Log (porcentagem)
 }
